ad6188d7f2d500f8dd86c56fe5a1fad5e37da77f
ロ　イみなみ <70193490+roiminami@users.noreply.github.com>
2026-07-07 09:38:28 +0900

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -5,6 +5,6 @@
 const toeicModulesConfig = [
     { id: 1, title: "第 01 - 10 词", unit: "g1-10", book: "gold" },
     { id: 2, title: "第 11 - 20 词", unit: "g11-20", book: "gold" },
-    { id: 3, title: "第 21 - 30 词", unit: "g1-10", book: "gold" },
-    { id: 4, title: "第 31 - 40 词", unit: "g11-20", book: "gold" }
+    { id: 3, title: "第 21 - 30 词", unit: "g21-30", book: "gold" },
+    { id: 4, title: "第 31 - 40 词", unit: "g31-40", book: "gold" }
 ];
